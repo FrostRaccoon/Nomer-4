1cd124fe21d1a47fc84954ec01d9486691cc944e
EnotWhyNot <danik_maltsev@mail.ru>
2023-06-23 22:27:37 +0300

    Get_new

--- script.js
+++ script.js
@@ -198,4 +198,51 @@ const arr = [2, 5, 9, 0, 3, 1, 4];
 for (let i = 0; i < arr.length && arr[i] !== 0; i++) {  //Второй вариант (плохочитаем)
   console.log(arr[i]);
 }
+
+
+Задание 5:
+ 
+1. Создать массив из 10 случайных чисел от 0 до 100 включительно.
+Наобходимо вывести в консоль все числа, которые делятся и на 2, и на 3.
+ 
 */
+ 
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+// }
+ 
+// const arr = [];
+ 
+// for (let i = 0; i < 10; i++) {
+//   arr.push(getRandomInt(25, 100));
+// }
+ 
+// console.log(arr);
+ 
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0 && arr[i] % 3 === 0) {
+//     console.log(arr[i]);
+//   }
+// }
+ 
+// 2. Дан массив: `[1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3]`. Подсчитайте количество
+// цифр 3 в этом массиве.
+ 
+// const arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
+// let counter = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 3) {
+//     counter++;
+//   }
+// }
+// console.log(counter);
+ 
+// 4. Дан массив: `[1, 2, 3, 4, 5]`.
+// С помощью метода splice преобразуйте массив в следующий: [1, 4, 5]
+ 
+// const arr = [1, 2, 3, 4, 5];
+// arr.splice(1, 2, 100, 101, 2003);
+// console.log(arr);
+// Тайминг: 25 минут.
